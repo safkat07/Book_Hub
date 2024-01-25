1e757a00ccfb7507119db9013c2c9c803b3abc3e
safkat07 <safkatsadli@gmail.com>
2024-01-25 15:22:07 +0600

    banner section new design updated with 100% responsiveness

--- src/Pages/Banner/Banner.jsx
+++ src/Pages/Banner/Banner.jsx
@@ -1,6 +1,51 @@
 import { Link } from "react-router-dom";
+// img import
+import img1 from '../../assets/Banner_Books/banner_book_1.jpg'
+import img2 from '../../assets/Banner_Books/banner_book_2.jpg'
+import img3 from '../../assets/Banner_Books/banner_book_3.jpg'
+import img4 from '../../assets/Banner_Books/banner_book_4.jpg'
 import './banner.css'
+import { useState } from "react";
+
+const ImgList = [
+  {
+    "id": 1,
+    "img": img1,
+    "bookTitle": "The Girl with the Dragon Tattoo",
+    "bookDescription": "A gripping mystery thriller that follows investigative journalist Mikael Blomkvist and hacker Lisbeth Salander as they uncover dark secrets and conspiracies.",
+    "bookAuthor": "Stieg Larsson"
+  },
+  {
+    "id": 2,
+    "img": img2,
+    "bookTitle": "The Color Purple",
+    "bookDescription": "A poignant novel that explores the life of Celie, an African-American woman in the early 20th century, addressing themes of racism, sexism, and the power of sisterhood.",
+    "bookAuthor": "Alice Walker"
+  },
+
+  {
+    "id": 3,
+    "img": img3,
+    "bookTitle": "Pother Pachali",
+    "bookDescription": "A classic Bengali novel capturing the struggles and aspirations of a poor family in rural Bengal. It reflects on life's challenges and the pursuit of dreams.",
+    "bookAuthor": "B. Bandyopadhyay"
+  },
+  {
+    "id": 4,
+    "img": img4,
+    "bookTitle": "Aryonnok",
+    "bookDescription": "An epic fantasy tale set in a magical world, following the journey of a young hero as he discovers his destiny and confronts ancient evils threatening the land.",
+    "bookAuthor": "Humayun Ahmed"
+  },
+
+
+]
 const Banner = () => {
+  // chnage book img
+  const [bookID, setBookID] = useState(img1)
+  const [bookTitle, setBookTitle] = useState("The Girl With The Dragon Tatto")
+  const [bookDescription, setBookDescription] = useState("A gripping mystery thriller that follows investigative journalist Mikael Blomkvist and hacker Lisbeth Salander as they uncover dark secrets and conspiracies.")
+  const [bookAuthor, setBookAuthor] = useState("Stieg Larsson")
   return (
     // <section className="h-screen -mt-[4.4rem] bg-gradient-to-r from-orange-600 via-sky-600 to-black  ">
 
@@ -12,7 +57,55 @@ const Banner = () => {
 
     // </section>
     <section className="bg h-screen">
-      
+      <div className="max-w-[1380px]   absolute xl:top-[20%] md:top-20 top-20 right-0 left-0 bottom-0 mx-auto">
+        <div className="flex w-full md:flex-row flex-col justify-center gap-x-20 items-center">
+          <div className=" text-black md:w-1/3 w-full md:px-0 px-4 font-semibold font-montserrat">
+            <p className="md:text-4xl text-2xl font-bold">
+              {bookTitle}
+            </p>
+            <p className="font-semibold font-poppins text-white bg-gradient-to-r  from-orange-500 to-80%  py-2 px-2 rounded-xl w-3/4 md:text-2xl text-xl mt-2">
+              {bookAuthor}
+            </p>
+            <p className="md:text-xl mt-4">
+              {bookDescription}
+            </p>
+            {/* <Link to='/register'>
+              <button className="border-2 border-black 2xl:px-5 xl:px-3 px-2 xl:py-1.5 py-1 bg-black text-white rounded-md">Buy Now</button>
+            </Link> */}
+
+
+          </div>
+          <div className="">
+            <img className="md:w-80 w-60 md:h-[500px] rounded-bl-[5rem] rounded-tr-[5rem] rounded-xl " src={bookID} alt="" />
+          </div>
+          {/* little img listt */}
+
+        </div>
+        <div className=" max-w-[690px]  xl:mt-2 mt-0    mx-auto">
+          <div className="space-y-2 flex md:mr-12 justify-center gap-x-5">
+            {
+              ImgList.map(data => (
+                <img key={data.id} src={data.img}
+                  className="md:max-w-[120px] max-w-[70px] rounded-xl cursor-pointer hover:scale-105 duration-500  max-h-[200px]"
+                  onClick={() => {
+                    setBookID(
+                      data.id === 1 ? img1 :
+                        data.id === 2 ? img2 :
+                          data.id === 3 ? img3 :
+                            img4
+                    )
+                    setBookAuthor(data.bookAuthor)
+                    setBookDescription(data.bookDescription)
+                    setBookTitle(data.bookTitle)
+
+                  }}
+
+                />
+              ))
+            }
+          </div>
+        </div>
+      </div>
     </section>
 
   );
--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -26,12 +26,7 @@ const Navbar = () => {
   const { user, logOut } = useContext(AuthContext);
   const navigate = useNavigate();
 
-  useEffect(() => {
-    localStorage.setItem("theme", theme);
-    const addLocalTheme = localStorage.getItem("theme");
-    // add custom data-theme attribute to the html tag required to update the theme using DaisyUI
-    document.querySelector("html").setAttribute("data-theme", addLocalTheme);
-  }, [theme]);
+
 
   const handleSingout = () => {
     logOut().then(() => {
@@ -46,12 +41,12 @@ const Navbar = () => {
       <div className="flex justify-between items-center">
         {/* icons div */}
         <Link>
-            <p className="font-rubik 2xl:text-5xl xl:text-4xl lg:text-3xl md:text-3xl text-2xl font-semibold tracking-[.2rem]">BookHub</p>
+          <p className="font-rubik 2xl:text-5xl xl:text-4xl lg:text-3xl md:text-3xl text-2xl font-semibold tracking-[.2rem]">BookHub</p>
         </Link>
         {/* icons div */}
         {/* Navigation Div */}
         <div className="lg:block hidden">
-          <ul className="flex bg-gradient-to-tl  xl:gap-x-7 gap-x-3 xl:text-xl text-lg font-poppins  font-medium">
+          <ul className="flex bg-gradient-to-tl  xl:gap-x-7 gap-x-3 xl:text-2xl text-lg font-poppins  font-medium">
             <Link><li className="  hover:text-indigo-500 transition-all duration-500">Home</li></Link>
             <Link><li className="  hover:text-indigo-500 transition-all duration-500">All Books</li></Link>
             <Link><li className="  hover:text-indigo-500 transition-all duration-500">About Us</li></Link>
@@ -61,15 +56,24 @@ const Navbar = () => {
         </div>
         {/* Navigation Div */}
         {/* Social Icons */}
-        <div className="lg:flex text-xl items-center justify-center gap-x-3 hidden">
-          <button className="border-2 border-black 2xl:px-5 xl:px-3 px-2  xl:py-1.5 py-1 rounded-md">Login</button>
-          <button className="border-2 border-black 2xl:px-5 xl:px-3 px-2 xl:py-1.5 py-1 bg-black text-white rounded-md">Sign-up</button>
+        <div className="lg:flex font-montserrat font-semibold text-xl items-center justify-center gap-x-3 hidden">
+          <Link to='/login'>
+            <button className="border-2 border-black 2xl:px-7 xl:px-5 px-4  xl:py-1.5 py-1 rounded-md">Login</button>
+          </Link>
+          <Link to='/register'>
+            <button className="border-2 border-black 2xl:px-5 xl:px-3 px-2 xl:py-1.5 py-1 bg-black text-white rounded-md">Sign-up</button>
+          </Link>
         </div>
         {/* Social Icons */}
+        {/* respnsive deisgn */}
+        <div className="block lg:hidden">
+          ham
+        </div>
       </div>
+
     </nav>
     // <nav>
-      
+
     // </nav>
   );
 };
